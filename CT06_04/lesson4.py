7e42599ecd3474f83cc03758695ce7b8fcd8d851
trainer1-computhink <trainer1@computhink.com.sg>
2026-01-28 08:58:00 +0000

    week 4

--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -1,1 +1,9 @@
 print("Hello from lesson 4")
+
+# answer = input("What is the day of the month today")
+# print(type(answer))
+# num1 = int(input("give me number 1: "))
+# num2 = input("give me number 2: ")
+
+# print(int(num1) + int(num2))
+print(2 + 2.5)
--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -1,1 +1,19 @@
 print("Hello from lesson 5")
+
+# num = 1
+# for i in range(1,21,2):
+#     num = num + i
+#     print(i)
+#     print(num)
+
+# name = input("What is your name?")
+# for char in name:
+#     print("Give me a " + char)
+
+num = 0
+for i in range(1,21,2):
+    num = num + i
+    print(num)
+
+1 + 3 = 4
+4 + 5 = 9
