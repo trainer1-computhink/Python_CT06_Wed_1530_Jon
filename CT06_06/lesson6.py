c05d3c0b8a0a5e561f49a9f0127b668ef1e5abc5
trainer1-computhink <trainer1@computhink.com.sg>
2026-02-09 22:40:07 +0800

    Update lesson6.py

--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -1,1 +1,17 @@
 print("Hello from lesson 6")
+
+# Debugging question 1
+total = "0"
+
+for i in range(1, 4)
+    score = input("Enter score " + i + ": ")
+    total = total + score
+
+print("Total score is: " + totl)
+
+# Debugging question 2
+name = input("Enter your name: ")
+
+for i in range(5):
+    message = i + ". " + Name
+    print(messages)
