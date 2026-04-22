2d8674aeabbdb053d521dabe6430925f5ca913d9
trainer1-computhink <trainer1@computhink.com.sg>
2026-04-22 14:26:21 +0800

    Create end_sem_assessment_revision.py

---
+++ CT06_14/end_sem_assessment_revision.py
@@ -0,0 +1,160 @@
+# Qns 1 Lists
+
+groceries = ["apple", "banana", "coconut", "pineapple", "pear"]
+
+# 1. Remove the item "banana" from the list.
+
+# 2. After removing it, print the 2nd item in the list.
+
+# 3. Add "dragon fruit" so that it appears immediately before "pineapple".
+#    (You are not allowed to hardcode the index)
+
+# 4. Ask the user to continuously input grocery items.
+#    Each item should be added to the END of the list.
+#    Stop when the user types "end".
+
+# 5. Print all items in the list in this format:
+#    1. apple
+#    2. coconut
+#    ...
+
+# 6. Generate a random index based on the current list size.
+#    Print the item at that position, then remove it from the list.
+
+# 7. Ask the user to search for an item.
+
+#    If found:
+#    Print:
+#    "[item] is found at position [index]"
+
+#    If not found:
+#    Print:
+#    "[item] is not in the list"
+
+
+# Qns 2 If - elif - else and order of operations
+
+# A reward system works as follows:
+
+# 1. If the score is NOT between 0 and 100, print "Invalid score"
+
+# 2. If the player is NOT banned AND score is above 89
+#    OR the player is a member AND score is above 95,
+#    print "Diamond reward"
+
+# 3. If the score is above 74 AND (the player is a member OR has finished the game),
+#    print "Gold reward"
+
+# 4. If the score is above 49 AND the player is NOT banned,
+#    print "Silver reward"
+
+# 5. If the player is banned OR has NOT finished the game,
+#    print "No reward"
+
+# 6. Otherwise, print "Bronze reward"
+
+# Ask the user for:
+# - score
+# - member status ("yes" / "no")
+# - banned status ("yes" / "no")
+# - finished game status ("yes" / "no")
+
+# Important:
+# - You must use and, or, not
+# - You must use brackets where needed
+# - The order of checking matters
+
+# Test your program with:
+
+# Case 1:
+# score = 96, member = no, banned = no, finished = yes
+# Expected: Diamond reward
+
+# Case 2:
+# score = 80, member = no, banned = no, finished = yes
+# Expected: Gold reward
+
+# Case 3:
+# score = 80, member = no, banned = no, finished = no
+# Expected: Silver reward
+
+# Case 4:
+# score = 60, member = no, banned = yes, finished = yes
+# Expected: No reward
+
+# Qns 3: Debug the Code
+
+# This program should:
+# 1. Ask the user to enter numbers
+# 2. Stop when the user types "end"
+# 3. Store ONLY numbers greater than 10
+# 4. Print the total and average at the end
+# 5. You are NOT allowed to remove any lines.
+# 6. You can only MODIFY existing lines.
+
+# copy and paste to start finish
+# numbers = []
+
+# while True
+#     value = input("Enter number: ")
+
+#     if value == "end":
+#         break
+
+#     if int(value) > 10:
+#         numbers.append(value)
+
+# total = 0
+
+# for i in range(len(number)):
+#     total = total + numbers[i]
+
+# average = total / len(numbers)
+
+# print("Total is: " + total)
+# print("Average is: " + average)
+
+
+
+# Qns 4: Debug the Code
+
+# This program should:
+# 1. Keep asking the user for a score
+# 2. Stop when the user types "end"
+# 3. Convert each valid score into an AL level
+# 4. Store all AL results in a list
+# 5. Print the final list
+# 6. You are NOT allowed to remove any lines.
+# 7. You can only MODIFY existing lines.
+
+# al_list = []
+
+# while True
+# score = input("Enter score: ")
+
+#     if score == "end"
+#         break
+
+#     if score < 0 or score > 100:
+#         print("Invalid")
+
+#     elif score > 89:
+#         al = 1
+#     elif score > 84:
+#         al = 2
+#     elif score > 79:
+#         al = 3
+#     elif score > 74:
+#         al = 4
+#     elif score > 64:
+#         al = 5
+#     elif score > 44:
+#         al = 6
+#     elif score > 19:
+#         al = 7
+#     else:
+#         al = 8
+
+#     al_list.append(a1)
+
+# print("AL list: " + al_list)
