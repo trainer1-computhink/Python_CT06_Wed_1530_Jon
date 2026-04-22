bcb25107a183e119ddde12095622ac4bf0989698
trainer1-computhink <trainer1@computhink.com.sg>
2026-04-22 16:48:05 +0800

    Update lesson14.py

--- CT06_14/lesson14.py
+++ CT06_14/lesson14.py
@@ -1,1 +1,32 @@
-print("Hello from lesson 15")
+# Task 3a
+# Create a program that:
+# - add items to a shopping list
+# - calculate their costs
+# - print a receipt
+
+# 1) Print the current shopping list.
+# 2) Ask the user how many more items they want to buy.
+# 3) Use a for loop to ask what the items are and append it to the shopping list.
+# 4)Print the updated shopping list.
+
+# Task 3b
+# 1) Create an empty list to store the prices: price_list
+
+# 2) For each item in the shopping list:
+#    a) Ask for price
+#    b) Ask for quantity
+#    c) Multiply the price and quantity and append it to the price list
+
+# 3) Print the price list
+
+# Task 3c
+# 1) Use a for loop to print the shopping list and price list following the format below.
+# 2) Add up the total cost in the price list and print it.
+
+# ---ORDER SUMMARY---
+# Item 1: $XXX
+# Item 2: $YYY
+# ...
+# Item Last: $ZZZ
+# --------------------
+# Total: $AAA
